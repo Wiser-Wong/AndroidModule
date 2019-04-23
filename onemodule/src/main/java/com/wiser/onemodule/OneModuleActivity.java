a465b1331bcbc328677e607ccbebf9264203ba15
Wiser-Wong <Wiser_XY_Wong@163.com>
2019-04-23 16:12:30 +0800

    Android 组件化开发模板

--- onemodule/src/main/java/com/wiser/onemodule/OneModuleActivity.java
+++ onemodule/src/main/java/com/wiser/onemodule/OneModuleActivity.java
@@ -1,38 +1,43 @@
 package com.wiser.onemodule;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
+
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.wiser.library.base.WISERActivity;
 import com.wiser.library.base.WISERBuilder;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+import org.greenrobot.eventbus.EventBus;
 
-@Route(path = "/OneModule",group = "one")
-public class OneModuleActivity extends WISERActivity implements View.OnClickListener{
+@Route(path = "/OneModule", group = "one")
+public class OneModuleActivity extends WISERActivity implements View.OnClickListener {
 
-	TextView tvSkip;
+    TextView tvSkip;
 
-	@Override protected WISERBuilder build(WISERBuilder wiserBuilder) {
-		wiserBuilder.layoutId(R.layout.activity_one_module);
-		wiserBuilder.swipeBack(true);
-		return wiserBuilder;
-	}
+    @Override
+    protected WISERBuilder build(WISERBuilder wiserBuilder) {
+        wiserBuilder.layoutId(R.layout.activity_one_module);
+        wiserBuilder.swipeBack(true);
+        return wiserBuilder;
+    }
 
-	@Override protected void initData(Bundle bundle) {
-		tvSkip = findViewById(R.id.tv_skip);
-		tvSkip.setOnClickListener(this);
-	}
+    @Override
+    protected void initData(Intent intent) {
+        tvSkip = findViewById(R.id.tv_skip);
+        tvSkip.setOnClickListener(this);
+    }
 
-	@Override
-	public void onClick(View v) {
-		int i = v.getId();
-		if (i == R.id.tv_skip) {
-//			Intent intent = new Intent();
-//			intent.setClassName(this,"com.wiser.twomodule.TwoModuleActivity");
-//			startActivity(intent);
-			ARouter.getInstance().build("/two/TwoModule").navigation();
-		}
-	}
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.tv_skip) {
+            // Intent intent = new Intent();
+            // intent.setClassName(this,"com.wiser.twomodule.TwoModuleActivity");
+            // startActivity(intent);
+            EventBus.getDefault().postSticky("测试EventBus");
+            ARouter.getInstance().build("/two/TwoModule").navigation();
+        }
+    }
 }
--- twomodule/src/main/java/com/wiser/twomodule/TwoModuleActivity.java
+++ twomodule/src/main/java/com/wiser/twomodule/TwoModuleActivity.java
@@ -3,18 +3,41 @@ package com.wiser.twomodule;
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.wiser.library.base.WISERActivity;
 import com.wiser.library.base.WISERBuilder;
+import com.wiser.library.helper.WISERHelper;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 @Route(path = "/two/TwoModule")
 public class TwoModuleActivity extends WISERActivity {
 
-	@Override protected WISERBuilder build(WISERBuilder wiserBuilder) {
-		wiserBuilder.layoutId(R.layout.activity_two_module);
-		return wiserBuilder;
-	}
+    @Override
+    protected WISERBuilder build(WISERBuilder wiserBuilder) {
+        wiserBuilder.layoutId(R.layout.activity_two_module);
+        return wiserBuilder;
+    }
 
-	@Override protected void initData(Bundle bundle) {
+    @Override
+    protected void initData(Intent intent) {
+        if (!EventBus.getDefault().isRegistered(this))
+            EventBus.getDefault().register(this);
+    }
 
-	}
+    // 测试EventBus
+    @Subscribe(sticky = true)
+    public void testEventBus(String s) {
+        WISERHelper.toast().show(s);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (EventBus.getDefault().isRegistered(this))
+            EventBus.getDefault().unregister(this);
+    }
 }
